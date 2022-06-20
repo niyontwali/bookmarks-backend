641e6fb0b00a987e0a6388cb12b686be42ae48e7
John Niyontwali <nijohn0006@gmail.com>
2022-06-20 07:16:24 +0200

    Added desc to arrange the order from new to olg

--- src/graphql/Bookmark.ts
+++ src/graphql/Bookmark.ts
@@ -1,50 +1,55 @@
-import { extendType, nonNull, objectType, intArg, stringArg  } from 'nexus';
+import { extendType, nonNull, objectType, intArg, stringArg } from "nexus";
 
 export const Bookmark = objectType({
   name: "Bookmark",
   definition(t) {
     t.nonNull.int("id");
     t.nonNull.string("title");
-    t.nonNull.string('description');
+    t.nonNull.string("description");
     t.nonNull.string("link");
     t.nonNull.string("category");
-  }
-})
+  },
+});
 
 // Query
 
 export const BookmarkQuery = extendType({
-  type: 'Query',
+  type: "Query",
   definition(t) {
     t.nonNull.list.nonNull.field("allBookmarks", {
-      type: 'Bookmark',
+      type: "Bookmark",
       resolve(parent, args, context, info) {
-        return context.prisma.bookmark.findMany()
-      }
+        return context.prisma.bookmark.findMany({
+          orderBy: [
+            {
+              id: "desc",
+            },
+          ],
+        });
+      },
     }),
-     // find one bookmark
-   t.field('oneBookmark', {
-    type: "Bookmark",
-    args: {
-      id: nonNull(intArg())
-    },
-    resolve (parent, args, context) {
-      return context.prisma.bookmark.findUnique({
-        where: {
-          id: args.id
+      // find one bookmark
+      t.field("oneBookmark", {
+        type: "Bookmark",
+        args: {
+          id: nonNull(intArg()),
         },
-      })
-    }
-  })
-  }
-})
+        resolve(parent, args, context) {
+          return context.prisma.bookmark.findUnique({
+            where: {
+              id: args.id,
+            },
+          });
+        },
+      });
+  },
+});
 
 // Mutation
 
 export const bookmarkMutation = extendType({
   type: "Mutation",
   definition(t) {
-
     // creating a bookmark
     t.nonNull.field("createBookmark", {
       type: "Bookmark",
@@ -52,7 +57,7 @@ export const bookmarkMutation = extendType({
         title: nonNull(stringArg()),
         description: nonNull(stringArg()),
         link: nonNull(stringArg()),
-        category: nonNull(stringArg())
+        category: nonNull(stringArg()),
       },
       resolve(parent, args, context) {
         const { title, description, link, category } = args;
@@ -61,60 +66,54 @@ export const bookmarkMutation = extendType({
             title,
             description,
             link,
-            category
-          }
+            category,
+          },
         });
         return newBookmark;
-      }
-    })
+      },
+    });
 
     // upating a bookmark
-    t.nonNull.field('updateBookmark', {
+    t.nonNull.field("updateBookmark", {
       type: "Bookmark",
       args: {
         id: nonNull(intArg()),
         title: nonNull(stringArg()),
         description: nonNull(stringArg()),
         link: nonNull(stringArg()),
-        category: nonNull(stringArg())
-
+        category: nonNull(stringArg()),
       },
       resolve(parent, args, context) {
-        const {title, description, link, category, id } = args;
+        const { title, description, link, category, id } = args;
         const updatedBookmark = context.prisma.bookmark.update({
           where: {
-            id
+            id,
           },
           data: {
             title,
             description,
             link,
-            category
-          }
-        })
-        return updatedBookmark
-      }
-    })
+            category,
+          },
+        });
+        return updatedBookmark;
+      },
+    });
 
     // deleting a bookmark
-    t.nonNull.field('deleteBookmark', {
-      type: 'Bookmark',
+    t.nonNull.field("deleteBookmark", {
+      type: "Bookmark",
       args: {
-        id: nonNull(intArg())
+        id: nonNull(intArg()),
       },
       resolve(parent, args, context) {
         const { id } = args;
         return context.prisma.bookmark.delete({
           where: {
-            id
-          }
-        })
-      }
-    })
-
-  }
-})
-
-
-
-
+            id,
+          },
+        });
+      },
+    });
+  },
+});
